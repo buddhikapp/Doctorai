57ca78b9f3757c01a73044f9e05fbd04e4457998
mudithalasantha <mudithalasantha@gmail.com>
2016-11-26 00:18:50 +0530

    t

--- app.py
+++ app.py
@@ -72,13 +72,13 @@ def webhook():
                             search_result = search.search_symtom_limit(message, 5)
 #                            for i in range(len(search_result)):
 #                                log(str(i) + " " + str(search_result[i]))
-                            log("-----------" + str(search_result))
-#                            if len(search_result) > 0:
-#                                my_dict = json.loads(str(search_result))
-#                                log("----- : "+ my_dict[0])
+                            log("----------- " + str(search_result))
+                            if len(search_result) > 0:
+                                sid = str(search_result[0]["id"])
+                                log("************ " + sid)
+#                                send_message(sender_id,search_result)
 
-                                           #sid = str(search_result[0])
-                                #send_message(sender_id,search_result)
+                                    
 #                        elif string.find(message,"headache") is not -1:
 #                            sid = diagnose.searchSymptom("headache", sender_id)
 #                            send_message(sender_id, "Give me a sec!")
