044e83feb4827ab8f9c89826d12bf4308b2ad2ec
mudithalasantha <mudithalasantha@gmail.com>
2016-11-25 23:36:13 +0530

    tt2

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ def webhook():
                             search_result = search.search_symtom_limit(message, 5)
                             for i in range(len(search_result)):
                                 log(str(i) + " " + str(search_result[i]))
-                            if len(search_result) > 0
+                            if len(search_result) > 0:
                                 my_dict = json.loads(str(search_result))
                                 log("----- : "+ my_dict[0])
 
