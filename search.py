dc159d9a130dda4c796598ef2d22a6816e357441
mudithalasantha <mudithalasantha@gmail.com>
2016-11-25 13:38:19 +0530

    change search

--- search.py
+++ search.py
@@ -10,20 +10,20 @@ print(api.info())
 def search_symtom(phrase):
 	return api.search(phrase)
 
-def search_symtom(phrase, limit):
+def search_symtom_limit(phrase, limit):
     return api.search(phrase, max_results=limit)
 
-def search_symtom(phrase, gender):
+def search_symtom_gender(phrase, gender):
     return api.search(phrase, sex=gender)
 
-def search_symtom(phrase, gender, limit):
+def search_symtom_gender_limit(phrase, gender, limit):
     return api.search(phrase, sex=gender, max_results=limit)
 
-def search_symtom(phrase, search_filters):
+def search_symtom_filter(phrase, search_filters):
     return api.search(phrase, filters=search_filters)
 
-def search_symtom(phrase, search_filters, gender):
+def search_symtom_filter_gender(phrase, search_filters, gender):
     return api.search(phrase, filters=search_filters, sex=gender)
 
-def search_symtom(phrase, search_filters, gender, limit):
+def search_symtom_filter_gender_limit(phrase, search_filters, gender, limit):
     return api.search(phrase, filters=search_filters, sex=gender, max_results=limit)
