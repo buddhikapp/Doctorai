950849370f8a50e85419cafe4389d9a9f770affe
mudithalasantha <mudithalasantha@gmail.com>
2016-11-25 16:24:05 +0530

    test

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def webhook():
                         else:
                             send_message(sender_id, "Give me a sec!")
                             search_result = search.search_symtom_limit(message, 5)
-                            for i in range(len(search_result):
+                            for i in range(len(search_result)):
                                 log(i, search_result[i])
 
                                            #sid = str(search_result[0])
