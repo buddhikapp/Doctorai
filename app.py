1d9ef34065647e6955bb588a7cca292537e14256
mudithalasantha <mudithalasantha@gmail.com>
2016-11-25 23:25:04 +0530

    test

--- app.py
+++ app.py
@@ -72,8 +72,8 @@ def webhook():
                             search_result = search.search_symtom_limit(message, 5)
                             for i in range(len(search_result)):
                                 log(str(i) + " " + str(search_result[i]))
-                        my_dict = json.loads(str(search_result[0]))
-                        log("----- : "+ my_dict[0])
+                            my_dict = json.loads(str(search_result[0]))
+                            log("----- : "+ my_dict[0])
 
                                            #sid = str(search_result[0])
                                 #send_message(sender_id,search_result)
