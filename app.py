422e0399ac8f3f29563793e197168c9cf85cb996
mudithalasantha <mudithalasantha@gmail.com>
2016-12-04 16:16:58 +0530

    multi user added

--- app.py
+++ app.py
@@ -154,6 +154,7 @@ def webhook():
                         if attach["type"] == "location":
                             latitude = attach["payload"]["coordinates"]["lat"]
                             longitude = attach["payload"]["coordinates"]["long"]
+                            
                             clinic_type = "hospital"
                             clinicsURL = "https://api.foursquare.com/v2/venues/search?ll="+str(latitude)+","+str(longitude)+"&radius=15000&query="+clinic_type+"&client_id=1TCDH3ZYXC3NYNCRVL1RL4WEGDP4CHZSLPMKGCBIHAYYVJWA&client_secret=VASKTPATQLSPXIFJZQ0EZ4GDH2QAZU1QGEEZ4YDCKYA11V2J&v=20160917"
                             r = urllib.urlopen(clinicsURL)
